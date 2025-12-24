27affd09f90514fa87ce6aefd678cb3eb9ff6d71
davydshcherba <davidserba302@gmail.com>
2025-12-24 14:32:53 +0200

    add authx and authx configuration in main and route login and update readme

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base,Mapped, mapped_column
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -8,8 +9,16 @@ if not DATABASE_URL:
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
     DB_NAME = os.getenv("DB_NAME", "app_db")
-    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_PORT = os.getenv("DB_PORT", "5434")
     DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from authx import AuthX, AuthXConfig
+from .core.database import Base, engine, User
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+config = AuthXConfig(
+    JWT_ALGORITHM="HS256",
+    JWT_SECRET_KEY="SECRET_KEY",  
+    JWT_TOKEN_LOCATION=["headers"],
+)
+
+auth = AuthX(config=config)
 app = FastAPI(title="My API")
+Base.metadata.create_all(engine)
 
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/login')  
+def login(username: str, password: str, db: Session = Depends(get_db)):
+    stmt = select(User).where(User.username == username)
+    user = db.scalar(stmt)
+    
+    if user and user.hashed_password == password:
+        token = auth.create_access_token(uid=username)
+        return {"access_token": token}
+    
+    raise HTTPException(status_code=401, detail={"message": "Invalid credentials"})
