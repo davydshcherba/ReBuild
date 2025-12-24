18414e01ad63a4efaa96c4c55813ff9595927b09
davydshcherba <davidserba302@gmail.com>
2025-12-24 14:49:36 +0200

    add hashing password and login with hashing password

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from authx import AuthX, AuthXConfig
 from .core.database import Base, engine, User
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 config = AuthXConfig(
     JWT_ALGORITHM="HS256",
@@ -13,7 +14,7 @@ config = AuthXConfig(
 auth = AuthX(config=config)
 app = FastAPI(title="My API")
 Base.metadata.create_all(engine)
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_db():
     db = Session(engine)
@@ -28,12 +29,17 @@ def login(username: str, password: str, db: Session = Depends(get_db)):
     stmt = select(User).where(User.username == username)
     user = db.scalar(stmt)
     
-    if user and user.hashed_password == password:
+    if not user:
+        raise HTTPException(status_code=401, detail={"message": "Invalid credentials"})
+    
+    password_bytes = password.encode("utf-8")[:72]
+    is_correct_password = pwd_context.verify(password_bytes, user.hashed_password)
+
+    if is_correct_password:
         token = auth.create_access_token(uid=username)
         return {"access_token": token}
     
     raise HTTPException(status_code=401, detail={"message": "Invalid credentials"})
-
 @app.post("/register")
 def register(username: str, email: str, password: str, db: Session = Depends(get_db)):
     stmt = select(User).where(User.username == username)
@@ -42,7 +48,24 @@ def register(username: str, email: str, password: str, db: Session = Depends(get
     if existing_user:
         raise HTTPException(status_code=400, detail={"message": "Username already exists"})
     
-    new_user = User(username=username, hashed_password=password, email=email)
+    password_bytes = password.encode("utf-8")[:72]
+    hashed_password = pwd_context.hash(password_bytes)
+
+    
+    new_user = User(username=username, hashed_password=hashed_password, email=email)
+    db.add(new_user)
+    db.commit()
+    return {"message": "User registered successfully"}
+
+    stmt = select(User).where(User.username == username)
+    existing_user = db.scalar(stmt)
+    
+    if existing_user:
+        raise HTTPException(status_code=400, detail={"message": "Username already exists"})
+    
+    password_bytes = password.encode('utf-8')[:72]
+    hashed_password = pwd_context.hash(password_bytes)
+    new_user = User(username=username, hashed_password=hashed_password, email=email)
     db.add(new_user)
     db.commit()
     return {"message": "User registered successfully"}
